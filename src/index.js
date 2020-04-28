ca1d59ac1086b28e047b0ceb9aa69c803b54c313
JessicaIreland <rollwithitbakery@hotmail.com>
2020-04-28 08:41:07 -0400

    todoList

--- src/index.js
+++ src/index.js
@@ -22,11 +22,45 @@ function renderTodoApp() {
     let app = document.querySelector("#app")
     let h1 = document.createElement("h1")
     // your code here
+    // const listNode = document.querySelector(".list")
+    let listNode = document.createElement("form")
+    // listNode.classList.add(".list")
 
-    h1.innerText = "Todo List"
+  
+
+    // for (let list of todos.list){}
+        
+        let html = `
+        <ul style="list-style-type: none;">
+            <li class="list-item">
+            <input type ="checkbox"/>
+         Take Jim to the hair salon
+            </li>
+            <li class="list-item">
+            <input type ="checkbox"/>
+         Drop off vedding invitation at mailbox
+            </li>
+            <li class="list-item">
+            <input type ="checkbox"/>
+         Pick up the cake
+            </li>
+            <li class="list-item">
+            <input type ="checkbox"/>
+         Call the caterers
+            </li>
+        </ul>    
+            `;
+        listNode.innerHTML += html    
+   
+    
+    
+    
+
+    h1.innerText = "To Do List"
 
     app.appendChild(h1)
     // and maybe some here
+    app.appendChild(listNode)
 }
 
 renderTodoApp()
